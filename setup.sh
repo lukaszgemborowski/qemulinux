b3752f7ad60ca6ea531a0354db278cabc03149c6
Lukasz Gemborowski <lukasz.gemborowski@gmail.com>
2016-06-24 22:34:19 +0200

    buildroot support

--- setup.sh
+++ setup.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-KERNEL_URL="https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.4.6.tar.xz"
 QEMU_URL="http://wiki.qemu-project.org/download/qemu-2.5.1.tar.bz2"
-BUSYBOX_URL="http://sources.openelec.tv/mirror/busybox/busybox-1.24.1.tar.bz2"
+BUILDROOT_URL="https://buildroot.org/downloads/buildroot-2016.05.tar.bz2"
 
-LINUX_PKG="linux-4.4.6.tar.xz"
 QEMU_PKG="qemu-2.5.1.tar.bz2"
-BUSYBOX_PKG="busybox-1.24.1.tar.bz2"
+BUILDROOT_PKG="buildroot-2016.05.tar.bz2"
 
 if [ -z "$CC_PREFIX" ]
 then
@@ -23,55 +21,22 @@ fi
 function prepare_dirs {
 	mkdir -p srcdir
 	mkdir -p src
-	mkdir -p tmp/linux
-	mkdir -p tmp/busybox
 	mkdir -p tmp/qemu
         mkdir -p tmp/initramfs
 }
 
 function download_all {
 	cd srcdir
-	if [ ! -f ${LINUX_PKG} ]; then wget ${KERNEL_URL}; fi
 	if [ ! -f ${QEMU_PKG} ]; then wget ${QEMU_URL}; fi
-	if [ ! -f ${BUSYBOX_PKG} ]; then wget ${BUSYBOX_URL}; fi
+	if [ ! -f ${BUILDROOT_PKG} ]; then wget ${BUILDROOT_URL}; fi
 	cd ..
 }
 
 function unpack_all {
 	cd src
-	if [ ! -d "linux" ]; then tar xvf ../srcdir/${LINUX_PKG} && mv linux-4.4.6 linux; fi
-        if [ ! -d "busybox" ]; then tar xvf ../srcdir/${BUSYBOX_PKG} && mv busybox-1.24.1 busybox; fi
         if [ ! -d "qemu" ]; then tar xvf ../srcdir/${QEMU_PKG} && mv qemu-2.5.1 qemu; fi
+		if [ ! -d "buildroot" ]; then tar xvf ../srcdir/${BUILDROOT_PKG} && mv buildroot-2016.05 buildroot; fi
         cd ..
-}
-
-function build_linux {
-        cd src/linux
-	make O=../../tmp/linux ARCH=arm CROSS_COMPILE=$CC_PREFIX -j8
-        cd -
-}
-
-function menuconfig_linux {
-        cd src/linux
-        make O=../../tmp/linux ARCH=arm menuconfig
-        cd -
-}
-
-function configure_linux {
-        cp config tmp/linux/.config
-}
-
-function configure_busybox {
-        cd src/busybox
-        make defconfig O=../../tmp/busybox ARCH=arm CROSS_COMPILE=$CC_PREFIX
-        echo "CONFIG_STATIC=y" >> ../../tmp/busybox/.config
-        cd -
-}
-
-function build_busybox {
-        cd src/busybox
-        make install -j8 O=../../tmp/busybox ARCH=arm CROSS_COMPILE=$CC_PREFIX
-        cd -
 }
 
 function configure_qemu {
@@ -87,40 +52,28 @@ function build_qemu {
         cd -
 }
 
-function build_initramfs {
-        cd tmp/initramfs
-        mkdir -pv {bin,sbin,etc,proc,sys,usr/{bin,sbin}}
-        cp ../../init .
-        chmod +x init
-        cp -av ../../tmp/busybox/_install/* .
-        find . -print0 \
-             | cpio --null -ov --format=newc \
-             | gzip -9 > ../initramfs.cpio.gz
-        cd -
+function build_system {
+	mkdir -p tmp/buildroot
+	cp buildroot.config tmp/buildroot/.config
+
+	cd src/buildroot
+	make O=../../tmp/buildroot
+	cd -
 }
 
-
-
 function make_all {
-        configure_linux
-        configure_busybox
         configure_qemu
-
-        build_linux
-        build_busybox
         build_qemu
-        build_initramfs
+        build_system
 }
 
 function run_qemu {
-        tmp/qemu/bin/qemu-system-arm -machine versatilepb -kernel tmp/linux/arch/arm/boot/zImage \
-                -initrd tmp/initramfs.cpio.gz -append "root=/dev/mem" \
-                -dtb tmp/linux/arch/arm/boot/dts/versatile-pb.dtb \
+        tmp/qemu/bin/qemu-system-arm -machine versatilepb -kernel tmp/buildroot/images/zImage \
+                -initrd tmp/buildroot/images/rootfs.cpio -append "root=/dev/mem" \
+                -dtb tmp/buildroot/images/versatile-pb.dtb \
                 -serial stdio
 }
-
 
 prepare_dirs
 download_all
 unpack_all
-
